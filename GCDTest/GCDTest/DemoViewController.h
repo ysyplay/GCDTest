72105c8c23b292cb445dcbc636bfe070972c7a07
牧羊的诗人 <1094510449@qq.com>
2020-08-15 15:30:31 +0800

    Add files via upload

---
+++ GCDTest/GCDTest/DemoViewController.h
@@ -0,0 +1,15 @@
+//
+//  DemoViewController.h
+//  GCDTest
+//
+//  Created by LH on 8/15/20.
+//  Copyright © 2020 ysyplay. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+
+//测试方法名
+@property (nonatomic,copy) NSString *selectorStr;
+@end
---
+++ GCDTest/GCDTest/GCDTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCDTableViewController.h
+//  GCDTest
+//
+//  Created by LH on 8/15/20.
+//  Copyright © 2020 ysyplay. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface GCDTableViewController : UITableViewController
+
+
+@end
